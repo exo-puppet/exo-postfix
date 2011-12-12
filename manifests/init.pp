0d6ee029d8c06e4fd7897cf77c267f7594e22382
Arnaud Héritier <aheritier@exoplatform.com>
2011-12-12 18:49:28 +0100

    show in doc how to set a value which isn't the default one

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,7 +24,7 @@
 # == Examples
 #
 #   class { "postfix":
-#       lastversion => false,
+#       lastversion => true,
 #   }
 #
 ################################################################################
